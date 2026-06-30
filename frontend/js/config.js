1c25e89dfa031cb62b5959c66ae154756007a33e
Newcityvip <mdrobiulislam@casinomcw.com>
2026-06-30 14:46:14 +0530

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.AffiliateSuccessConfig = Object.freeze({
-    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbzS1BSIB9sJV2OurR-7vDMAVaIsOuNGrwQocoM0B7n7wmg3N-PTqaQobrY869pdpSSHHg/exec'
+    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbx1z7NjKBK-K67M_ZEMDNFrkEdkPHzhdds90SlLs7spIx6uPtBSRxL4XXNXLH8rS3z38Q/exec'
   });
 })(window);
