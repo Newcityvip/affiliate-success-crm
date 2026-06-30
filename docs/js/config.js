7157954d63f2bc10907be6bb7a046d41e5f08648
Mirrob <mrobiul349@gmail.com>
2026-06-30 16:11:20 +0530

    fix: sync frontend API config
    # Conflicts:
    #	frontend/js/config.js

--- docs/js/config.js
+++ docs/js/config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.AffiliateSuccessConfig = Object.freeze({
-    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbwhGC2LL2EOsKamBqT_neWoSxNJ4AKwWWhTCicP43W47Jhry8VUBGuyMCG-4eDejHsT/exec'
+    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbx1z7NjKBK-K67M_ZEMDNFrkEdkPHzhdds90SILs7spIx6uPtBSRxL4XXNXLH8rS3z38Q/exec'
   });
 })(window);
--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.AffiliateSuccessConfig = Object.freeze({
-    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbx1z7NjKBK-K67M_ZEMDNFrkEdkPHzhdds90SlLs7spIx6uPtBSRxL4XXNXLH8rS3z38Q/exec'
+    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbx1z7NjKBK-K67M_ZEMDNFrkEdkPHzhdds90SILs7spIx6uPtBSRxL4XXNXLH8rS3z38Q/exec'
   });
 })(window);
